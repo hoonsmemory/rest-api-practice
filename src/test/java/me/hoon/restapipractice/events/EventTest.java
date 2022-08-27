0fcf58363e2ff688c71286a1d4ff0eca784b9f2c
hoon <dltjdgnsok1@naver.com>
2022-08-28 00:23:12 +0900

    금액 무료, 장소 체크 메서드 테스트

--- src/test/java/me/hoon/restapipractice/events/EventTest.java
+++ src/test/java/me/hoon/restapipractice/events/EventTest.java
@@ -17,4 +17,56 @@ public class EventTest {
 
         assertThat(event).isNotNull();
     }
+
+    @Test
+    public void freeTest() {
+        //basePrice = 0, maxPrice = 0
+        Event event1 = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+
+        event1.checkFreeAndLocation();
+        assertThat(event1.isFree()).isTrue();
+
+
+        //basePrice = 100, maxPrice = 0
+        Event event2 = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+
+        event2.checkFreeAndLocation();
+        assertThat(event2.isFree()).isFalse();
+
+
+        //basePrice = 0, maxPrice = 1000
+        Event event3 = Event.builder()
+                .basePrice(0)
+                .maxPrice(1000)
+                .build();
+
+        event3.checkFreeAndLocation();
+        assertThat(event3.isFree()).isFalse();
+    }
+
+    @Test
+    public void offlineTest() {
+        Event event1 = Event.builder()
+                .basePrice(0)
+                .maxPrice(1000)
+                .build();
+
+        event1.checkFreeAndLocation();
+        assertThat(event1.isOffline()).isTrue();
+
+        Event event2 = Event.builder()
+                .basePrice(0)
+                .maxPrice(1000)
+                .location("강남역 3번 출구")
+                .build();
+
+        event2.checkFreeAndLocation();
+        assertThat(event2.isOffline()).isFalse();
+    }
 }
