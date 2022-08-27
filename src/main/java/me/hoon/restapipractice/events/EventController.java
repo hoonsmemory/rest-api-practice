3f690b78eb14b9c2ed5784ede38194a48e50c865
hoon <dltjdgnsok1@naver.com>
2022-08-27 22:48:31 +0900

    eventValidator 추가

--- src/main/java/me/hoon/restapipractice/events/EventController.java
+++ src/main/java/me/hoon/restapipractice/events/EventController.java
@@ -32,12 +32,12 @@ public class EventController {
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
 
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         eventValidator.eventCreateValidate(eventDto, errors);
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
 
