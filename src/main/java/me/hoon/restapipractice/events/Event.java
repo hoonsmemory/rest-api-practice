ba24e6cb3831e75ad3c0f83d805f6c4a76987537
hoon <dltjdgnsok1@naver.com>
2022-08-28 00:22:52 +0900

    금액 무료, 장소 체크 메서드 구현

--- src/main/java/me/hoon/restapipractice/events/Event.java
+++ src/main/java/me/hoon/restapipractice/events/Event.java
@@ -36,4 +36,19 @@ public class Event {
     //ORDINAL 을 사용할 경우 enum 의 순서가 바뀔 시 값도 변경될 수 있으므로 STRING 으로 사용하는게 좋다.
     @Enumerated(value = EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
+
+    //금액 무료, 장소 체크
+    public void checkFreeAndLocation() {
+        if(this.basePrice == 0 && this.maxPrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+
+        if(this.location == null || this.location.isBlank()) {
+            this.offline = false;
+        } else {
+            this.free = true;
+        }
+    }
 }
