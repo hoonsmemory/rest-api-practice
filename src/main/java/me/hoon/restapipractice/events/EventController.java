4ef6be6cc247700777eb27ece82d9df64547c38c
hoon <dltjdgnsok1@naver.com>
2022-08-29 16:43:37 +0900

    Event 수정, 테스트 구현

--- src/main/java/me/hoon/restapipractice/events/EventController.java
+++ src/main/java/me/hoon/restapipractice/events/EventController.java
@@ -7,12 +7,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -40,12 +38,12 @@ public class EventController {
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
 
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(new ErrorsResource(errors));
+            return badRequest(errors);
         }
 
-        eventValidator.eventCreateValidate(eventDto, errors);
+        eventValidator.validate(eventDto, errors);
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(new ErrorsResource(errors));
+            return badRequest(errors);
         }
 
 
@@ -83,5 +81,43 @@ public class EventController {
         }
 
         return ResponseEntity.ok(new EventResource(event.get()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity editEvent(@PathVariable("id") Integer id,
+                                    @RequestBody @Valid EventDto eventDto,
+                                    Errors errors) {
+
+        //id 에 해당하는 값이 있는지 체크
+        Optional<Event> optionalEvent = eventRepository.findById(id);
+        if(optionalEvent.isEmpty()) {
+            errors.reject("null.event.object", null);
+        }
+
+        //DTO Validation 체크
+        if(errors.hasErrors()) {
+            return badRequest(errors);
+        }
+
+        //Event Validation 체크
+        eventValidator.validate(eventDto, errors);
+        if(errors.hasErrors()) {
+            return badRequest(errors);
+        }
+
+        //eventDto to event
+        Event event = optionalEvent.get();
+        modelMapper.map(eventDto, event);
+
+        //save
+        Event savedEvent = eventRepository.save(event);
+
+        //return
+        EventResource eventResource = new EventResource(savedEvent);
+        return ResponseEntity.ok(eventResource);
+    }
+
+    private static ResponseEntity<ErrorsResource> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
--- src/test/java/me/hoon/restapipractice/events/EventControllerTests.java
+++ src/test/java/me/hoon/restapipractice/events/EventControllerTests.java
@@ -1,52 +1,21 @@
 package me.hoon.restapipractice.events;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
+import me.hoon.restapipractice.common.DefaultControllerTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@RunWith(SpringRunner.class)
-//@WebMvcTest //웹 관련 빈만 등록해 준다. (슬라이스), Repository 관련 Bean 은 Mocking, stubbing 해야한다.
-@SpringBootTest //Mocking 할 게 많으면 테스트하기 힘드므로 @SpringBootTest 를 사용해서 모든 빈을 등록시킨다.
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-public class EventControllerTests {
-
-    /**
-     * MockMvc 를 사용하면 mocking 이 되어있는 DispatcherServlet 을 상대로 가짜 요청, 응답을 확인하는 테스트가 가능하다.
-     * 단위테스트라고는 볼 수 없다.
-     */
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Autowired
-    ModelMapper modelMapper;
+public class EventControllerTests extends DefaultControllerTest {
 
     @Autowired
     EventRepository eventRepository;
@@ -192,6 +161,34 @@ public class EventControllerTests {
 
         Event event = modelMapper.map(eventDto, Event.class);
         eventRepository.save(event);
+    }
+
+    //Event 수정
+    @Test
+    public void editEvent() throws Exception {
+        createEvents(1);
+
+        EventDto eventDto = EventDto.builder()
+                .name("changeName")
+                .description("Event Bean create Test")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 8, 26, 14, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 9, 1, 15, 00))
+                .beginEventDateTime(LocalDateTime.of(2022, 8, 26, 14, 00))
+                .endEventDateTime(LocalDateTime.of(2022, 9, 1, 15, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("REST API 오프라인 강의")
+                .build();
+
+
+        mockMvc.perform(put("/api/events/{id}",1)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(objectMapper.writeValueAsString(eventDto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("changeName"));
+
     }
 
 
