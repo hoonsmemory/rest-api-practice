1113131b639f72aeb8d8687aeddd217ff995fa1b
leesh1993 <dltjdgnsok1@naver.com>
2022-09-13 16:58:48 +0900

    시큐리티 기본 설정

---
+++ src/main/java/me/hoon/restapipractice/configs/SecurityConfig.java
@@ -0,0 +1,71 @@
+package me.hoon.restapipractice.configs;
+
+import me.hoon.restapipractice.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    /*
+        AuthorizationServer 랑 ResourceServer 에서
+        해당 AuthenticationManager 를 참조할 수 있도록 bean 으로 노출
+     */
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //스프링 시큐리티가 사용자를 인증하는 방법이 담긴 객체
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    //스프링 시큐리티를 적용하면 모든 요청은 스프링 필터를 거쳐야한다.
+    //스프링 필터를 무시할 경로 지정
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    //인증, 인가에 대한 세부적인 보안 기능을 설정할 수 있는 API 제공
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .anonymous()
+                .and()
+                .formLogin()
+                .and()
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/api/**").authenticated()
+                .anyRequest().authenticated();
+    }
+}
